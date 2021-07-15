7872acda6e9ad1c3bffc49cdc33572820c2ed6d0
kuccho524 <kucchachachanocha@gmail.com>
2021-07-15 17:34:24 +0900

    practice escape word

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello {
   public static void main(String args[]) {
-    System.out.println("Hello World");
+    System.out.println("Hello World\nIt's Thursday today!");
     System.out.println("こんにちは、JAVA");
   }
 }
